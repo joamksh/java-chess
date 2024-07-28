9130b4f8fa80b3cc0aa9dee612b0a92accf396e6
joamksh <joamksh@naver.com>
2024-07-28 17:07:42 +0900

    test : source에서 target으로 값 이동

--- src/test/java/ChessBoardTest.java
+++ src/test/java/ChessBoardTest.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class ChessBoardTest {
     @Test
-    public void testInitializeBoard() {
+     void testInitializeBoard() {
         ChessBoard chessBoard = new ChessBoard();
         List<List<String>> board = chessBoard.getBoard();
 
@@ -28,10 +28,23 @@ public class ChessBoardTest {
     }
 
     @Test
-    public void testPrintBoard() {
+     void testPrintBoard() {
         ChessBoard chessBoard = new ChessBoard();
         chessBoard.printBoard();
         // 콘솔 출력을 수동으로 확인하거나 콘솔 출력을 캡처하고 assert 하는 라이브러리 사용
+    }
+
+    @Test
+    public void testMovePiece() {
+        ChessBoard chessBoard = new ChessBoard();
+        try {
+            chessBoard.movePiece("b2", "b3");
+            List<List<String>> board = chessBoard.getBoard();
+            assertEquals(".", board.get(6).get(1)); // 이전 위치가 비었는지 확인
+            assertEquals("p", board.get(5).get(1)); // 새로운 위치에 말이 이동했는지 확인
+        } catch (InvalidCommandException e) {
+            fail("유효한 이동에서 예외 발생: " + e.getMessage());
+        }
     }
 
 }
